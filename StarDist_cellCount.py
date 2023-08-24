5954fa864bcc4fadc3eaa26dbea7e27591d81921
Yashasvi Bajaj (Avi) <93557301+ybajaj-29@users.noreply.github.com>
2023-08-24 17:35:53 -0400

    Update StarDist_cellCount.py
    De-clutter console prints; dd output logging and progress bar, add StarDist parameters to command (i.e., defaults) to allow future modifications

--- StarDist_cellCount.py
+++ StarDist_cellCount.py
@@ -5,7 +5,7 @@
 
 #@ DatasetIOService io
 #@ CommandService command
-#@ UIService ui # for interactive debugging of label image segmentation with ui.show() calls
+#@ UIService ui  # for interactive debugging of label image segmentation with ui.show() calls
 
 from ij import IJ, WindowManager
 from ij.plugin.frame import RoiManager
@@ -14,25 +14,34 @@ from de.csbdresden.stardist import StarDist2D
 from glob import glob
 import os
 
-# Fetch the absolute paths of the source and 8-bit output directories
-srcDir = srcFile.getAbsolutePath()
-bit8Dir = bit8Dir.getAbsolutePath()
-labelDir = labelDir.getAbsolutePath()
+# Initialize logging
+log_file_path = os.path.join(labelDir.getAbsolutePath(), "proc_log.txt")
+log_file = open(log_file_path, "w")
+
+def log(message):
+    #print(message)  # Show message in the console
+    log_file.write(message + "\n")  # Write message to log file
+
+# Function to display progress bar
+def show_progress(current, total):
+    pct = (current / float(total)) * 100
+    print("Progress: {:.2f}%".format(pct))
 
 # Function to get the filename without the path
 def get_filename(file_path):
     return os.path.basename(file_path)
 
 # Function to convert images to 8-bit and save them
-def convert_to_8bit(inputDir, outputDir, prefix=""):
-    for f in sorted(glob(os.path.join(inputDir, "*" + ext))):
-        print("Converting to 8-bit: " + get_filename(f))
-
+def convert_to_8bit(inputDir, outputDir, srcDir, ext, prefix=""):
+    log("\n--- Converting to 8-bit ---")
+    files = sorted(glob(os.path.join(inputDir, "*" + ext)))
+    for idx, f in enumerate(files):
+        log("Converting to 8-bit: {}".format(get_filename(f)))
+        
         # Open the image
         imp = IJ.openImage(f)
-        
+
         # Minimize artifacts from microscopy lighting and set a global scale for accurate processing
-        # Only applied to images in the source directory
         if inputDir == srcDir:
             IJ.run(imp, "Subtract Background...", "rolling=10")
             IJ.run(imp, "Set Scale...", "distance=1.075 known=1 unit=µm global")
@@ -42,45 +51,54 @@ def convert_to_8bit(inputDir, outputDir, prefix=""):
         IJ.save(imp, os.path.join(outputDir, prefix + get_filename(f)))
 
 # Function to run StarDist on all 8-bit TIF files in the specified directory
-def batch_process_StarDist():
-    # Open the ROI Manager
+def batch_process_StarDist(bit8Dir, labelDir, ext, command):
+    log("\n--- Running StarDist ---")
     rm = RoiManager.getRoiManager()
     if rm is None:
         rm = RoiManager()
-    
-    # Open or create the .dat file for saving the results
+
     output_file_path = os.path.join(labelDir, "cell_counts.dat")
     with open(output_file_path, "w") as output_file:
-        for f in sorted(glob(os.path.join(bit8Dir, "*" + ext))):
-            print("Processing: " + get_filename(f))
+        files = sorted(glob(os.path.join(bit8Dir, "*" + ext)))
+        for idx, f in enumerate(files):
+            label_output_path = os.path.join(labelDir, "label_" + get_filename(f))
+            if os.path.exists(label_output_path):
+                log("Skipping {}, already processed.".format(get_filename(f)))
+                continue
+            
+            log("Processing: {}".format(get_filename(f)))
+            show_progress(idx + 1, len(files))
 
-            # Open the image
             imp = IJ.openImage(f)
-
-            # Run StarDist2D
-            res = command.run(StarDist2D, False,
+            res = command.run(
+            				  StarDist2D, False,
                               "input", imp,
                               "modelChoice", "Versatile (fluorescent nuclei)",
-                              "outputType", "Both").get()
+                              "outputType", "Both",
+                              "normalizeInput", True,  # default
+                              "percentileBottom", 1.0,  # default
+                              "percentileTop", 99.8,  # default
+                              "probThresh", 0.5,  # default
+                              "nmsThresh", 0.4  # default
+                             ).get()
 
-            # Retrieve the label image and save
             label = res.getOutput("label")
-            #ui.show(label)
-            io.save(label, os.path.join(labelDir, "label_" + get_filename(f)))
+            io.save(label, label_output_path)
 
-            # Count the number of ROIs (cells) and write the counts to a .dat file
             if rm is not None:
                 n_cells = rm.getCount()
-                output_file.write(get_filename(f) + ": " + str(n_cells) + "\n")
-
-                # Optionally, clear the ROI Manager for the next image
+                output_file.write("{}: {}\n".format(get_filename(f), str(n_cells)))
                 rm.reset()
 
-# Execute the conversion and batch processing
-convert_to_8bit(srcDir, bit8Dir, prefix="8bit_")
-batch_process_StarDist()
-convert_to_8bit(labelDir, labelDir)
+if __name__ == "__main__":
+    srcDir = srcFile.getAbsolutePath()
+    bit8Dir = bit8Dir.getAbsolutePath()
+    labelDir = labelDir.getAbsolutePath()
 
-# Close the ROI Manager window at the end of the batch processing
-if WindowManager.getFrame("ROI Manager") is not None:
+    convert_to_8bit(srcDir, bit8Dir, srcDir, ext, prefix="8bit_")
+    batch_process_StarDist(bit8Dir, labelDir, ext, command)
+    convert_to_8bit(labelDir, labelDir, srcDir, ext)
+    
     WindowManager.getFrame("ROI Manager").close()
+
+    log_file.close()
